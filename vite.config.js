8020449b4dc0b07801e1626f485963ca9160f330
Roshan Kumar <rsahu7989@gmail.com>
2026-03-10 14:07:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/Pizza-App-Arch/',  // ← ADD THIS (matches your homepage)
+ // base: '/Pizza-App-Arch/',  // ← ADD THIS (matches your homepage)
   plugins: [react()],
   resolve: {
     alias: {
